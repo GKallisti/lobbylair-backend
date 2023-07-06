d9c30d613837147f0efb98699516d674bd4d7fa4
GgKallisti <riptheraven@gmail.com>
2023-07-05 22:12:25 -0300

    F

--- src/controllers/payPalController.js
+++ src/controllers/payPalController.js
@@ -88,14 +88,7 @@ const captureOrder = async (req, res) => {
     await payment.setUser(userId);
 
     const user = await User.findByPk(userId);
-    await User.update( {
-      isPremium: true
-      
-    } , {
-      where: {
-        id: userId,
-      },
-    })
+ 
 
     const subscription = await Subscriptions.create({
       type: type,
